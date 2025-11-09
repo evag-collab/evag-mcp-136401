ec3ac2f948f0700b9be6441d782ed15cd3497939
evag-collab <evlassisbics@gmail.com>
2025-11-09 11:14:11 +0200

    Update hierarchy.js

--- tools/evag-mcp-test/evag-mcp-test-collection-v-1/hierarchy.js
+++ tools/evag-mcp-test/evag-mcp-test-collection-v-1/hierarchy.js
@@ -10,7 +10,7 @@ const executeFunction = async ({ accountId }) => {
   const token = process.env.EVAG_MCP_TEST_API_KEY;
   try {
     // Construct the URL for the request
-    const apiUrl = `${url}/api/accountHierarchy`);
+    const apiUrl = new URL(`https://${url}/api/accountHierarchy`);
 
     // Set up headers for the request
     const headers = {
